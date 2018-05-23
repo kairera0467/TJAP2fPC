670218f066cad83e2faf55b6bda6f190b8ab2725
AioiLight <info@aioilight.space>
2018-05-23 14:12:25 +0900

    ランナーの表示位置を、X座標から求める

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏DrumsRunner.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏DrumsRunner.cs
@@ -20,16 +20,16 @@ namespace DTXMania
         private int[] Size = new int[] { 124, 250 };
         private int Ptn = 24;
         private int Chara = 4;
-        private int[] StartPoint = new int[] { 185, -50 };
+        private int[] StartPoint = new int[] { 175, -40 };
 
         public void Start(int Player, bool IsMiss, CDTX.CChip pChip)
         {
-            if(CDTXMania.Tx.Runner != null)
+            if (CDTXMania.Tx.Runner != null)
             {
                 for (int i = 0; i < 128; i++)
                 {
                     if (pChip.nチャンネル番号 < 0x15 || (pChip.nチャンネル番号 >= 0x1A))
-                        {
+                    {
                         if (!stRunners[i].b使用中)
                         {
                             stRunners[i].b使用中 = true;
@@ -43,7 +43,6 @@ namespace DTXMania
                                 stRunners[i].nType = random.Next(1, Chara + 1);
                             }
                             stRunners[i].ct進行 = new CCounter(0, 1280, 16, CDTXMania.Timer);
-                            stRunners[i].ctPtn = new CCounter(0, Ptn, 90, CDTXMania.Timer);
                             stRunners[i].nOldValue = 0;
                             stRunners[i].fX = 0;
                             break;
@@ -60,7 +59,6 @@ namespace DTXMania
                 stRunners[i] = new STRunner();
                 stRunners[i].b使用中 = false;
                 stRunners[i].ct進行 = new CCounter();
-                stRunners[i].ctPtn = new CCounter();
             }
             base.On活性化();
         }
@@ -70,7 +68,6 @@ namespace DTXMania
             for (int i = 0; i < 128; i++)
             {
                 stRunners[i].ct進行 = null;
-                stRunners[i].ctPtn = null;
             }
             base.On非活性化();
         }
@@ -89,11 +86,10 @@ namespace DTXMania
         {
             for (int i = 0; i < 128; i++)
             {
-                if(stRunners[i].b使用中)
+                if (stRunners[i].b使用中)
                 {
                     stRunners[i].nOldValue = stRunners[i].ct進行.n現在の値;
                     stRunners[i].ct進行.t進行();
-                    stRunners[i].ctPtn.t進行();
                     if (stRunners[i].ct進行.b終了値に達した || stRunners[i].fX > 1280)
                     {
                         stRunners[i].ct進行.t停止();
@@ -101,11 +97,13 @@ namespace DTXMania
                     }
                     if (stRunners[i].nOldValue < stRunners[i].ct進行.n現在の値)
                     {
-                        stRunners[i].fX += (float)CDTXMania.stage演奏ドラム画面.actPlayInfo.dbBPM / 20;
+                        stRunners[i].fX += (float)CDTXMania.stage演奏ドラム画面.actPlayInfo.dbBPM / 18;
+                        int Width = 1280 / Ptn;
+                        stRunners[i].nNowPtn = (int)stRunners[i].fX / Width;
                     }
-                    if(CDTXMania.Tx.Runner != null)
+                    if (CDTXMania.Tx.Runner != null)
                     {
-                        CDTXMania.Tx.Runner.t2D描画(CDTXMania.app.Device, (StartPoint[0] + stRunners[i].fX), StartPoint[1], new Rectangle(stRunners[i].ctPtn.n現在の値 * Size[0], stRunners[i].nType * Size[1], Size[0], Size[1]));
+                        CDTXMania.Tx.Runner.t2D描画(CDTXMania.app.Device, (StartPoint[0] + stRunners[i].fX), StartPoint[1], new Rectangle(stRunners[i].nNowPtn * Size[0], stRunners[i].nType * Size[1], Size[0], Size[1]));
                     }
                 }
             }
@@ -122,12 +120,13 @@ namespace DTXMania
             public int nPlayer;
             public int nType;
             public int nOldValue;
+            public int nNowPtn;
             public float fX;
             public CCounter ct進行;
-            public CCounter ctPtn;
         }
         private STRunner[] stRunners = new STRunner[128];
         Random random = new Random();
+        int Oldbpm = 180;
         //-----------------
         #endregion
     }
