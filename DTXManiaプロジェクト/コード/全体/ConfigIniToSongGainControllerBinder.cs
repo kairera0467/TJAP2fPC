cd36713e281167c95c19c232c116c54466a09dab
twopointzero <jeremy@jeremygray.ca>
2018-08-23 07:29:29 -0700

    Fixed a couple of typographical errors in XML doc comments.

--- DTXManiaプロジェクト/コード/全体/ConfigIniToSongGainControllerBinder.cs
+++ DTXManiaプロジェクト/コード/全体/ConfigIniToSongGainControllerBinder.cs
@@ -5,9 +5,9 @@ namespace DTXMania
     /// <summary>
     /// The ConfigIniToSongGainControllerBinder allows for SONGVOL and/or other
     /// properties related to the Gain levels applied to song preview and
-    /// playback, to be applied conditional based on settings flowing from
+    /// playback, to be applied conditionally based on settings flowing from
     /// ConfigIni. This binder class allows that to take place without either
-    /// ConfigIni or SongGainController having awareness of one nother.
+    /// ConfigIni or SongGainController having awareness of one another.
     /// See those classes properties, methods, and events for more details. 
     /// </summary>
     internal static class ConfigIniToSongGainControllerBinder
