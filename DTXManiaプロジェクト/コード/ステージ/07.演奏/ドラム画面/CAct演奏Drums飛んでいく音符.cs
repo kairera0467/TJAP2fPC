44267397d38f48a92331f835e74feeda75d84aa0
AioiLight <info@aioilight.space>
2018-11-03 13:10:53 +0900

    不要なメソッドの削除

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏Drums飛んでいく音符.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏Drums飛んでいく音符.cs
@@ -17,58 +17,6 @@ namespace DTXMania
 			base.b活性化してない = true;
 		}
 		
-		
-		// メソッド
-        public virtual void Start( int nLane, int nPlayer )
-		{
-            if (CDTXMania.Tx.Notes != null)
-            {
-                for (int i = 0; i < 1; i++)
-                {
-                    for (int j = 0; j < 64; j++)
-                    {
-                        if (!this.st飛び散るチップ[j].b使用中)
-                        {
-                            this.st飛び散るチップ[j].b使用中 = true;
-                            int n回転初期値 = 1;
-                            double num7 = 1.115 + (1 / 100.0); // 拡散の大きさ
-                            this.st飛び散るチップ[j].nLane = (int)nLane;
-                            this.st飛び散るチップ[j].nPlayer = nPlayer;
-                            this.st飛び散るチップ[j].ct進行 = new CCounter(0, 323/4, 5, CDTXMania.Timer); // カウンタ
-
-                            this.st飛び散るチップ[j].fXL = (414 - 655) + (130 / 2); //X座標
-                            this.st飛び散るチップ[j].fXR = (414 - 655) + (130 / 2); //X座標
-
-                            this.st飛び散るチップ[j].fY = nPlayer == 0 ? (257 - 360) : ( 470 - 360 );
-                            this.st飛び散るチップ[j].f加速度X = (float)(num7 * Math.Cos((Math.PI * 2 * n回転初期値) / 360.0) + 2.53);
-                            this.st飛び散るチップ[j].f加速度Y = (float)(num7 * (Math.Sin((Math.PI * 2 * n回転初期値) / 360.0) - 1.3));
-                            this.st飛び散るチップ[j].f加速度の加速度X = 1.0027f;
-                            this.st飛び散るチップ[j].f加速度の加速度Y = 1.005f;
-                            this.st飛び散るチップ[j].f重力加速度 = 0.0347f;
-                            this.st飛び散るチップ[j].f半径 = (float)(0.6 + (((double)CDTXMania.Random.Next(30)) / 100.0));
-                            break;
-                        }
-
-                        //if( !this.st飛んで行く音符[j].b使用中 )
-                        //{
-                        //    this.st飛んで行く音符[j].b使用中 = true;
-
-                        //    this.st飛んで行く音符[j].nLane = (int)nLane;
-                        //    this.st飛んで行く音符[j].ct進行 = new CCounter(0, 49, 12, CDTXMania.Timer); // カウンタ
-
-                        //    this.st飛んで行く音符[j].fXL = CDTXMania.Skin.nScrollFieldX; //X座標
-                        //    this.st飛んで行く音符[j].fY = 257 - 65;
-
-                        //    int n頂点 = -18;
-                        //    this.st飛んで行く音符[j].f加速度X = (float)( ( 1224 - CDTXMania.Skin.nScrollFieldX ) / 50.0f );
-                        //    this.st飛んで行く音符[j].f加速度Y = (float)( ( 275 ) / ( 53.0f / 2.0f ) );// JLinePosY = 257
-                        //    break;
-                        //}
-                    }
-                }
-            }
-		}
-
         public virtual void t虹( int player )
 		{
             if (CDTXMania.Tx.Effects_Rainbow != null)
@@ -100,45 +48,24 @@ namespace DTXMania
 		{
             for( int i = 0; i < 2; i++ )
 			{
-				this.st状態[ i ].ct進行 = new CCounter();
 				this.st虹[ i ].ct進行 = new CCounter();
 				this.st虹2[ i ].ct進行 = new CCounter();
 			}
-            for (int i = 0; i < 64; i++)
-            {
-                this.st飛び散るチップ[i] = new ST飛び散るチップ();
-                this.st飛び散るチップ[i].b使用中 = false;
-                this.st飛び散るチップ[i].ct進行 = new CCounter();
-
-                this.st飛んで行く音符[i] = new ST飛び散るチップ();
-                this.st飛んで行く音符[i].b使用中 = false;
-                this.st飛んで行く音符[i].ct進行 = new CCounter();
-            }
             base.On活性化();
 		}
 		public override void On非活性化()
 		{
             for( int i = 0; i < 2; i++ )
 			{
-				this.st状態[ i ].ct進行 = null;
 				this.st虹[ i ].ct進行 = null;
 				this.st虹2[ i ].ct進行 = null;
 			}
-
-            for (int i = 0; i < 64; i++)
-            {
-                this.st飛び散るチップ[i].ct進行 = null;
-
-                this.st飛んで行く音符[i].ct進行 = null;
-            }
 			base.On非活性化();
 		}
 		public override void OnManagedリソースの作成()
 		{
 			if( !base.b活性化してない )
 			{
-                //this.tx音符 = CDTXMania.tテクスチャの生成( CSkin.Path( @"Graphics\7_taiko_notes.png" ) );
-                //this.tx虹 = CDTXMania.tテクスチャの生成( CSkin.Path( @"Graphics\7_balloon_rainbow.png" ) );
 				base.OnManagedリソースの作成();
 			}
 		}
@@ -146,9 +73,6 @@ namespace DTXMania
 		{
 			if( !base.b活性化してない )
 			{
-				//CDTXMania.tテクスチャの解放( ref this.tx音符 );
-    //            CDTXMania.tテクスチャの解放( ref this.tx虹 );
-
 				base.OnManagedリソースの解放();
 			}
 		}
@@ -225,42 +149,6 @@ namespace DTXMania
 
 		#region [ private ]
 		//-----------------
-        //private CTexture tx音符;
-        //private CTexture tx虹;
-        protected STSTATUS[] st状態 = new STSTATUS[2];
-
-        [StructLayout(LayoutKind.Sequential)]
-        protected struct STSTATUS
-        {
-            public CCounter ct進行;
-            public E判定 judge;
-            public int nIsBig;
-            public int n透明度;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct ST飛び散るチップ
-        {
-            public int nLane;
-            public int nPlayer;
-            public bool b使用中;
-            public CCounter ct進行;
-            public int n前回のValue;
-            public float fXL;
-            public float fXR;
-            public float fY;
-            public float fチップの質量;
-            public float f初速度X;
-            public float f初速度Y;
-            public float f加速度X;
-            public float f加速度Y;
-            public float f加速度の加速度X;
-            public float f加速度の加速度Y;
-            public float f重力加速度;
-            public float f半径;
-            public float f回転単位;
-            public float f回転方向;
-        }
 
         [StructLayout(LayoutKind.Sequential)]
         private struct ST虹
@@ -273,8 +161,6 @@ namespace DTXMania
 
         private ST虹[] st虹 = new ST虹[2];
         private ST虹[] st虹2 = new ST虹[2];
-        private ST飛び散るチップ[] st飛び散るチップ = new ST飛び散るチップ[64];
-        private ST飛び散るチップ[] st飛んで行く音符 = new ST飛び散るチップ[64]; //2016.08.30 kairera0467 新メソッド
 
 		//-----------------
 		#endregion
