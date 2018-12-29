85b11b65d1ef9cb4bcad0c68f2f3524f8b407913
AioiLight <info@aioilight.space>
2018-12-30 00:17:02 +0900

    イーズアウトを追加

--- TJAPlayer3/Stages/02.Title/CStageタイトル.cs
+++ TJAPlayer3/Stages/02.Title/CStageタイトル.cs
@@ -46,7 +46,7 @@ namespace TJAPlayer3
 				Trace.TraceInformation( "タイトルステージの活性化を完了しました。" );
 				Trace.Unindent();
 			}
-            EaseIn = new Animations.EaseIn(0, 1280, 3000);
+            EaseOut = new Animations.EaseOut(0, 1280, 3000);
             Fader = new Animations.FadeIn(1000);
 		}
 		public override void On非活性化()
@@ -109,7 +109,7 @@ namespace TJAPlayer3
 					this.ctカーソルフラッシュ用.t開始( 0, 700, 5, TJAPlayer3.Timer );
 					this.ctカーソルフラッシュ用.n現在の値 = 100;
 					base.b初めての進行描画 = false;
-                    EaseIn.Start();
+                    EaseOut.Start();
                     Fader.Start();
 				}
 				//---------------------
@@ -188,12 +188,12 @@ namespace TJAPlayer3
 
                 // 描画
 
-                EaseIn.Tick();
+                EaseOut.Tick();
                 Fader.Tick();
                 //TJAPlayer3.Tx.Title_Background.n透明度 = (int)Fader.GetAnimation();
                 if (TJAPlayer3.Tx.Title_Background != null )
-                    TJAPlayer3.Tx.Title_Background.t2D描画( TJAPlayer3.app.Device, Convert.ToInt32(EaseIn.GetAnimation()), 0 );
-                Console.WriteLine(Convert.ToDouble(EaseIn.GetAnimation()));
+                    TJAPlayer3.Tx.Title_Background.t2D描画( TJAPlayer3.app.Device, Convert.ToInt32(EaseOut.GetAnimation()), 0 );
+                Console.WriteLine(Convert.ToDouble(EaseOut.GetAnimation()));
 
                 #region[ バージョン表示 ]
                 //string strVersion = "KTT:J:A:I:2017072200";
@@ -383,7 +383,7 @@ namespace TJAPlayer3
 		private int n現在のカーソル行;
         //private CTexture txメニュー;
         //private CTexture tx背景;
-        private Animations.EaseIn EaseIn;
+        private Animations.EaseOut EaseOut;
         private Animations.FadeIn Fader;
 	
 		private void tカーソルを下へ移動する()
