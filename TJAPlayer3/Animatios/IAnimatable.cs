fb33dc6ffefb2fd8c896314d0a2407fb40c1b6f5
AioiLight <info@aioilight.space>
2018-12-29 15:12:30 +0900

    アニメーションに使うインターフェイスの作成。

---
+++ TJAPlayer3/Animatios/IAnimatable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TJAPlayer3.Animatios
+{
+    /// <summary>
+    /// アニメーション インターフェイス。
+    /// </summary>
+    interface IAnimatable
+    {
+        /// <summary>
+        /// アニメーションを開始します。
+        /// </summary>
+        void Start();
+        /// <summary>
+        /// アニメーションを停止します。
+        /// </summary>
+        void Stop();
+        /// <summary>
+        /// アニメーションをリセットします。
+        /// </summary>
+        void Reset();
+        /// <summary>
+        /// アニメーションの進行を行います。
+        /// </summary>
+        /// <returns>アニメーションのパラメータ。実装するクラスによって異なる。</returns>
+        object Tick();
+    }
+}
